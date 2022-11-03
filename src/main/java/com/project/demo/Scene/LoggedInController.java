225c2ab9f5467e0cbf61c39e1c3ed5b663e22958
daanutu <61309221+daanutu@users.noreply.github.com>
2022-11-03 18:23:59 +0100

    Fin navbar

--- src/main/java/com/project/demo/Scene/LoggedInController.java
+++ src/main/java/com/project/demo/Scene/LoggedInController.java
@@ -35,23 +35,27 @@ public class LoggedInController  {
         DBUtils.changeScene(event, "login-view.fxml", "Login", null);
     }
 
+    public void Home(ActionEvent event) {
+        DBUtils.changeScene(event, "loggedin-view.fxml", "Home!", null);
+        //TODO add getUsername from previous fxml scene
+    }
     public void News(ActionEvent event) {
         DBUtils.changeScene(event, "news.fxml", "Manage news!", null);
         //TODO add getUsername from previous fxml scene
     }
 
     public void Utenti(ActionEvent event){
-        DBUtils.changeScene(event, "news.fxml", "Manage news!", null);
+        DBUtils.changeScene(event, "Utenti.fxml", "Manage user!", null);
         //TODO add getUsername from previous fxml scene
     }
 
     public void Commenti(ActionEvent event){
-        DBUtils.changeScene(event, "news.fxml", "Manage news!", null);
+        DBUtils.changeScene(event, "Commenti.fxml", "Manage comments!", null);
         //TODO add getUsername from previous fxml scene
     }
 
     public void importaEsporta(ActionEvent event){
-        DBUtils.changeScene(event, "news.fxml", "Manage news!", null);
+        DBUtils.changeScene(event, "importa-esporta.fxml", "Manage Import-Export!", null);
         //TODO add getUsername from previous fxml scene
     }
 
