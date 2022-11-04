f9f5babf909b5bdef1a4e0f67a66709e492bf668
Esther <giulianoesther@gmail.com>
2022-11-04 08:57:53 +0100

    .

--- src/main/java/com/project/demo/Scene/UserController.java
+++ src/main/java/com/project/demo/Scene/UserController.java
@@ -11,12 +11,13 @@ import javafx.stage.Stage;
 import java.util.ArrayList;
 
 public class UserController {
-
+    /* TODO tableview, + dentro id primo elemento tabella per aggiungere utente, controllo se l'utente effettivamente esista
+        su telegram?, aggiungi dati a table view (non c'e' limite alla quantita' di dati) collega scrollbar a table view!*/
     public void visualizzaUtenti(GridPane grid, int first){ //first = first element to show
         DBget dBget = new DBget();
         ArrayList<Utente> utenti = dBget.getUserList();
         System.out.println(utenti);
-        //TODO da adattare alla situazione
+        //TODO da adattare alla situazione, non servono tre casi ma un solo ciclo, forse
         int y = 1;
         if(utenti.size()<=10){
             for(int i = first; i< utenti.size(); i++){
